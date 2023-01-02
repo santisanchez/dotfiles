02c00a34ac33b1e46998a4e85623ffe3f4ec959d
santisanchez <santisanchez.1214@gmail.com>
2023-01-01 19:56:12 -0500

    Fixed C-Space issue

--- init.lua
+++ init.lua
@@ -442,8 +442,9 @@ cmp.setup {
   mapping = cmp.mapping.preset.insert {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete,
-    ['<CR>'] = cmp.mapping.confirm {
+    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete({
+      reason = cmp.ContextReason.Auto,
+    }), { "i", "c" }), ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
