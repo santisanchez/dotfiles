64bbd3fca3342c76a26cac7b1a577f22c4c0bba1
santisanchez <santisanchez.1214@gmail.com>
2023-01-08 12:40:02 -0500

    Removed commented things from null-ls

--- lua/config/null-ls_config.lua
+++ lua/config/null-ls_config.lua
@@ -5,8 +5,5 @@ null_ls.setup({
         laravel_actions.relationships,
         null_ls.builtins.code_actions.refactoring,
         null_ls.builtins.code_actions.gitsigns,
-        -- null_ls.builtins.formatting.stylua,
-        -- null_ls.builtins.diagnostics.eslint,
-        -- null_ls.builtins.completion.spell,
     },
 })
