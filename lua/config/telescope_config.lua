3c997db93ef0c954f1beccfade10aabb8f47dae7
santisanchez <santisanchez.1214@gmail.com>
2023-01-10 23:02:50 -0500

    Oldfiles mapped

--- lua/config/telescope_config.lua
+++ lua/config/telescope_config.lua
@@ -396,7 +396,7 @@ telescope_builtin.memo = function(opts)
 end
 
 -- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader>e', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
