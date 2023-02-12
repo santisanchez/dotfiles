11f301604d55ac9f70cc123be24320b972471c95
santisanchez <santisanchez.1214@gmail.com>
2023-02-12 18:33:01 -0500

    Added surround plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -148,6 +148,15 @@ local base_plugins = {
     event = "VimEnter"
   },
 
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
   -- Git related plugins
   { 'tpope/vim-fugitive', cmd = "Git" },
   {
